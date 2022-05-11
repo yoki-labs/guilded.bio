18b2524b6b87ea4ff49ebb58305361e65cc44b16
shay <bakkcat@gmail.com>
2022-05-12 01:21:11 +0200

    Rewrite styling with Tailwind

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className='text-center py-2 opacity-60 hover:opacity-100 transition-opacity'>
+      Created by <a
+        href='https://github.com/yoki-labs'
+        className='hover:underline'
+      >
+        Yoki Labs
+      </a>
+    </div>
+  )
+}

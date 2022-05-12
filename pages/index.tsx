5cc77a7b22e5a8c0383de7a023e69dc546451d94
zaida04 <nico.03727+github@gmail.com>
2022-05-11 21:53:42 -0400

    chore: hotfix for vercel deployments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "../hooks/useSession";
+import { signIn, signOut } from "next-auth/react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {

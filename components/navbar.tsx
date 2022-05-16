89e528985c7062570e8a4881a132c6b3dc4a815f
shay <bakkcat@gmail.com>
2022-05-17 01:40:44 +0200

    Log in button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+
+import Button from "../components/button"
 
 export default function Navbar() {
+  const {data: session} = useSession();
+
   return (
     <div className="bg-guilded-black absolute mx-auto object-top h-15 w-full p-3 flex">
       <div className="pl-2 md:pl-20 my-auto text-base sm:text-lg md:text-xl select-none">
@@ -10,6 +15,11 @@ export default function Navbar() {
             <span className="text-white">.bio</span>
           </a>
         </Link>
+      </div>
+      <div className="ml-auto my-auto">
+        {!session && (
+          <Button onClick={() => signIn()}>Log in</Button>
+        )}
       </div>
     </div>
   );

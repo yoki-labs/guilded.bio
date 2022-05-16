f44cc605c9ca00d1cb89f07324b04aea60864689
zaida04 <nico.03727+github@gmail.com>
2022-05-16 18:42:52 -0400

    chore: prettier

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,14 @@ import NextAuth from "next-auth";
 import Guilded from "next-auth-guilded";
 
 export default NextAuth({
-  secret: "SDUIAFHSDIUFHSD",
-  providers:
-    process.env.NODE_ENV === "development"
-      ? []
-      : [
-          Guilded({
-            clientId: process.env.GUILDED_OAUTH_ID!,
-            clientSecret: process.env.GUILDED_OAUTH_SECRET!,
-          }),
-        ],
+    secret: "SDUIAFHSDIUFHSD",
+    providers:
+        process.env.NODE_ENV === "development"
+            ? []
+            : [
+                  Guilded({
+                      clientId: process.env.GUILDED_OAUTH_ID!,
+                      clientSecret: process.env.GUILDED_OAUTH_SECRET!,
+                  }),
+              ],
 });

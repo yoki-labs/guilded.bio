f7fdd132eb306d82c93766c8c2b65a97c9e089cc
shay <bakkcat@gmail.com>
2022-05-17 00:27:43 +0200

    Use `Link`

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="bg-guilded-black absolute mx-auto object-top h-15 w-full p-3 flex">
       <div className="pl-2 md:pl-20 my-auto text-base sm:text-lg md:text-xl select-none">
-        <a href="/">
-          <span className="text-guilded-gilded">guilded</span>
-          <span className="text-white">.bio</span>
-        </a>
+        <Link href="/">
+          <a>
+            <span className="text-guilded-gilded">guilded</span>
+            <span className="text-white">.bio</span>
+          </a>
+        </Link>
       </div>
     </div>
   );

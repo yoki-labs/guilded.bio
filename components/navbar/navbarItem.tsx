07bb14e4a93fb28996a44dcb909e212495d0d7b5
zaida04 <nico.03727+github@gmail.com>
2022-05-16 23:02:33 -0400

    feat: finally centered button on mobile

--- components/navbar/navbarItem.tsx
+++ components/navbar/navbarItem.tsx
@@ -1,7 +1,7 @@
 export default function NavbarItem(props: { text: string; dest: string }) {
     return (
         <li>
-            <a href={props.dest} className="block py-2 pr-4 pl-3 md:p-0 lg:mt-0 md:text-lg text-guilded-gilded hover:text-white mr-4">
+            <a href={props.dest} className="block py-2 pl-3 md:p-0 lg:mt-0 md:text-lg text-guilded-gilded hover:text-white mr-4">
                 {props.text}
             </a>
         </li>

8c186efda5a7a3ba7d5ded5c5debd4919a5bbb41
zaida04 <nico.03727+github@gmail.com>
2022-05-17 21:06:20 -0400

    feat: accordian

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 import Button from "../button";
 import NavbarItem from "./navbarItem";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 export default function Navbar() {
     const { data: session } = useSession();
+    const [isDropdownActive, setIsDropdownActive] = useState(false);
 
     return (
         <nav className="rounded px-2 py-2.5 bg-guilded-black sm:px-4 w-full">
             <div className="container mx-auto flex flex-wrap items-center justify-between">
-                <div className="whitespace-nowrap pl-2 md:pl-20 my-auto text-3xl select-none">
+                <div className="whitespace-nowrap pl-2 md:pl-20 my-auto text-4xl md:text-3xl select-none">
                     <Link href="/">
                         <a>
                             <span className="text-guilded-gilded">guilded</span>
@@ -17,7 +19,15 @@ export default function Navbar() {
                         </a>
                     </Link>
                 </div>
-                <div className="w-full md:block md:w-auto" id="mobile-menu">
+                <div className="block sm:hidden pr-4" onClick={() => setIsDropdownActive(!isDropdownActive)}>
+                    <button className="flex items-center  px-3 py-2 border rounded text-guilded-gilded hover:text-white hover:border-white">
+                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>Menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        </svg>
+                    </button>
+                </div>
+                <div className={`w-full md:block md:w-auto ${isDropdownActive ? "block" : "hidden"}`} id="mobile-menu">
                     <ul className="mt-4 items-center flex flex-col md:mt-0 md:flex-row md:space-x-4 md:pr-16">
                         <NavbarItem text={"About"} dest={"/about"} />
                         <NavbarItem text={"Users"} dest={"/users"} />

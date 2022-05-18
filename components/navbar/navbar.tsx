3fa8a5137f71db2e1aea501c34f17ca03db747cd
zaida04 <nico.03727+github@gmail.com>
2022-05-17 22:54:47 -0400

    chore: prettier

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -32,35 +32,12 @@ export default function Navbar() {
                         <NavbarItem text={"About"} dest={"/about"} />
                         <NavbarItem text={"Users"} dest={"/users"} />
                         <NavbarItem text={"Community"} dest={"/community"} />
-                        <div className="py-2">{session ? <Button onClick={() => signOut()}>Log Out</Button> : <Button onClick={() => signIn()}>Log In</Button>}</div>
+                        <div className="py-2">
+                            {session ? <Button onClick={() => signOut()}>Log Out</Button> : <Button onClick={() => signIn()}>Log In</Button>}
+                        </div>
                     </ul>
                 </div>
             </div>
         </nav>
     );
-
-    // return (
-    //     <div className="bg-guilded-black justify-center overflow-hidden absolute mx-auto object-top h-15 w-full p-3 flex flex-col">
-    //         <div className="container mx-auto flex flex-wrap items-center justify-between">
-    //             <div className="self-center whitespace-nowrap pl-2 md:pl-20 my-auto text-base sm:text-lg md:text-xl select-none">
-    //                 <Link href="/">
-    //                     <a>
-    //                         <span className="text-guilded-gilded">guilded</span>
-    //                         <span className="text-white">.bio</span>
-    //                     </a>
-    //                 </Link>
-    //             </div>
-    //             <div className="w-full block flex-grow md:flex lg:items-center md:w-auto">
-    //                 <div className="flex flex-col sm:text-xs md:mt-0 md:flex-row md:space-x-8 md:text-sm">
-    //                     <NavbarItem text={"About"} dest={"/about"} />
-    //                     <NavbarItem text={"Users"} dest={"/users"} />
-    //                     <NavbarItem text={"Community"} dest={"/community"} />
-    //                 </div>
-    //                 <div>
-
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
 }

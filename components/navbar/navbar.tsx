305928704044f99fc54a1b5cb67745b5ac9e7dd3
zaida04 <nico.03727+github@gmail.com>
2022-05-17 22:51:23 -0400

    fix: accordian on medium devices

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
                         </a>
                     </Link>
                 </div>
-                <div className="block sm:hidden pr-4" onClick={() => setIsDropdownActive(!isDropdownActive)}>
+                <div className="block md:hidden pr-4" onClick={() => setIsDropdownActive(!isDropdownActive)}>
                     <button className="flex items-center  px-3 py-2 border rounded text-guilded-gilded hover:text-white hover:border-white">
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <title>Menu</title>

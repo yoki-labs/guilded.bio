bd3c6581bf2104d899ddefd6e1cf419ebcc6902c
zaida04 <nico.03727+github@gmail.com>
2022-05-18 21:33:35 -0400

    refactor: rename from giantRectangle to box

--- components/giantRectangle/giantRectangle.tsx
+++
@@ -1,3 +0,0 @@
-export default function GiantRectangle({ children }: { children: React.ReactNode }) {
-    return <div className="w-full h-105 bg-guilded-gilded">{children}</div>;
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
 
 import Footer from "../components/footer";
-import GiantRectangle from "../components/giantRectangle/giantRectangle";
+import Box from "../components/box";
 import Navbar from "../components/navbar/navbar";
-import YellowButton from "../components/buttons/yellowButton";
 import BlackButton from "../components/buttons/blackButton";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-    const { data: session } = useSession();
-
     return (
         <div className="bg-guilded-slate text-guilded-white">
             <Head>
@@ -19,7 +15,7 @@ const Home: NextPage = () => {
                 <meta name="description" content="A platform allowing users to share details about themselves with a simple link." />
             </Head>
             <Navbar />
-            <GiantRectangle>
+            <Box>
                 <div className="h-full px-2 mx-auto w-full text-center py-5 flex">
                     <div className="m-auto">
                         <h1 className="text-black text-6xl font-bold">Tell the world about yourself</h1>
@@ -34,7 +30,7 @@ const Home: NextPage = () => {
                         </div>
                     </div>
                 </div>
-            </GiantRectangle>
+            </Box>
             <div className="h-screen mx-auto w-full max-w-3xl text-center py-5 flex"></div>
             <Footer />
         </div>

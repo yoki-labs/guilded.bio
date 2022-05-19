f50284711f4ac746e3fedf58fab5e565e5373c80
shay <bakkcat@gmail.com>
2022-05-19 04:27:33 +0200

    refactor: remove box component, extra horizontal padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 import Footer from "../components/footer";
-import Box from "../components/box";
 import Navbar from "../components/navbar/navbar";
 import Button from "../components/button";
 import Link from "next/link";
@@ -15,22 +14,20 @@ const Home: NextPage = () => {
                 <meta name="description" content="A platform allowing users to share details about themselves with a simple link." />
             </Head>
             <Navbar />
-            <Box>
-                <div className="h-full px-2 mx-auto w-full text-center py-5 flex">
-                    <div className="m-auto">
-                        <h1 className="text-black text-6xl font-bold">Tell the world about yourself</h1>
-                        <div className="py-6">
-                            <Link href="/bios/create">
-                                <Button color="black">
-                                    <p className="font-semibold">
-                                        Create your own <span className="text-guilded-gilded">bio</span>
-                                    </p>
-                                </Button>
-                            </Link>
-                        </div>
+            <div className="w-full py-32 px-8 text-center bg-guilded-gilded">
+                <div className="m-auto">
+                    <h1 className="text-black text-6xl font-bold">Tell the world about yourself</h1>
+                    <div className="py-6">
+                        <Link href="/bios/create">
+                            <Button color="black">
+                                <p className="font-semibold">
+                                    Create your own <span className="text-guilded-gilded">bio</span>
+                                </p>
+                            </Button>
+                        </Link>
                     </div>
                 </div>
-            </Box>
+            </div>
             <div className="h-screen mx-auto w-full max-w-3xl text-center py-5 flex"></div>
             <Footer />
         </div>

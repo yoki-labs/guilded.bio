3602d6f6e1e25c31231edbe3194a304374685e12
shay <bakkcat@gmail.com>
2022-05-20 19:41:03 +0200

    chore: run prettier

--- components/profile/smallCard.tsx
+++ components/profile/smallCard.tsx
@@ -7,13 +7,7 @@ export default function SmallCard(props: { id: string; name: string; bio: string
             <a>
                 <div className="bg-guilded-slate rounded-xl p-5 h-fit max-w-md shadow-lg hover:shadow-sm hover:bg-[#24262d] transition">
                     <div className="flex">
-                        <Image
-                            src={props.iconURL}
-                            alt={`${props.name}'s avatar`}
-                            className="rounded-full my-auto shadow"
-                            height="80"
-                            width="80"
-                        />
+                        <Image src={props.iconURL} alt={`${props.name}'s avatar`} className="rounded-full my-auto shadow" height="80" width="80" />
                         <h1 className="my-auto ml-4 text-2xl md:text-3xl font-semibold">{props.name}</h1>
                     </div>
                     <hr className="mb-2 mt-3 border-guilded-gray border" />

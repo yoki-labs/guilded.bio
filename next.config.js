fa1db210a0939b5bf2137f7faaca2ec11626b210
zaida04 <nico.03727+github@gmail.com>
2022-05-20 00:08:16 -0400

    feat: base of bigcard and /users route

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-	redirects: async () => [
-		{
-			source: "/community",
-			destination: "https://www.guilded.gg/i/kjeQPrR2",
-			permanent: true
-		}
-	]
+    redirects: async () => [
+        {
+            source: "/community",
+            destination: "https://www.guilded.gg/i/kjeQPrR2",
+            permanent: true,
+        },
+        {
+            source: "/u/:userId",
+            destination: "/users/:userId",
+            permanent: true,
+        },
+    ],
 };
 
 module.exports = nextConfig;
---
+++ pages/users/[slug].tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import BigCard from "../../components/profile/bigCard";
+import prisma from "../../lib/prisma";
+
+const User: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Guilded.bio - About</title>
+            </Head>
+            <div className="bg-guilded-slate h-full w-full text-guilded-white">
+                <div className="mx-auto py-8 px-4 flex"></div>
+            </div>
+        </>
+    );
+};
+
+export default User;

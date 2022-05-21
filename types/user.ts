e8df615a16c1e5b4bf78f4aea4489b06e3345ec5
zaida04 <nico.03727+github@gmail.com>
2022-05-21 00:12:21 -0400

    types: APIUser from Guilded API

---
+++ types/user.ts
@@ -0,0 +1,33 @@
+export interface GuildedUser {
+    id: string;
+    name: string;
+    subdomain: string;
+    aliases: Alias[];
+    profilePictureSm: string;
+    profilePicture: string;
+    profilePictureLg: string;
+    profilePictureBlur: string;
+    profileBannerBlur: string;
+    profileBannerLg: string;
+    profileBannerSm: string;
+    aboutInfo: AboutInfo;
+    badges: string[];
+    createdAt: string;
+}
+
+export interface AboutInfo {
+    tagLine: string;
+}
+
+export interface Alias {
+    alias: string;
+    discriminator: null;
+    name: string;
+    createdAt: string;
+    userId: string;
+    gameId: number;
+    socialLinkSource: null;
+    editedAt: string;
+    socialLinkHandle: null;
+    playerInfo: null;
+}

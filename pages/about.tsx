7752957b92520c17556674ffe560dbe7884aeb10
shay <bakkcat@gmail.com>
2022-05-24 00:00:25 +0200

    better text formatting on /about - use dedicated `guilded-link` color - style discord.bio hyperlink - smaller text

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,24 +13,25 @@ const About: NextPage = () => {
                 </div>
             </div>
             <div className="bg-guilded-gray h-full text-guilded-white">
-                <div className="h-screen mx-auto w-full max-w-6xl font-medium text-2xl text-center py-8 px-4 flex">
+                <div className="h-screen mx-auto w-full max-w-6xl font-medium text-guilded-white text-xl text-center py-8 px-4 flex">
                     <p>
                         Guilded.bio was born out of a desire for one centralized place for people to express themselves. We found that the posts feature in
-                        Guilded simply didn&apos;t suffice as a proper place to introduce yourself for a variety of reasons. Posts were prone to getting buried,
+                        Guilded simply didn't suffice as a proper place to introduce yourself for a variety of reasons. Posts were prone to getting buried,
                         which made it difficult for users to find your introduction post. There was a lack of a per-server profile feature, allowing people to
                         disclose certain information to certain servers, and no place to put information like servers that you own, servers that you moderate,
-                        and more. We derive inspiration from other platforms like <a href="https://discord.bio">discord.bio</a>. We are built using{" "}
-                        <a className="text-guilded-gilded" href="https://nextjs.org/">
+                        and more. We derive inspiration from other platforms like <a className="text-guilded-link" href="https://discord.bio">discord.bio</a>.
+                        We are built using{" "}
+                        <a className="text-guilded-link" href="https://nextjs.org/">
                             Next.js
                         </a>
                         ,{" "}
-                        <a className="text-guilded-gilded" href="https://tailwindcss.com/">
+                        <a className="text-guilded-link" href="https://tailwindcss.com/">
                             Tailwind
                         </a>
                         , and{" "}
-                        <a className="text-guilded-gilded" href="https://reactjs.org/">
+                        <a className="text-guilded-link" href="https://reactjs.org/">
                             React
-                        </a>
+                        </a>.
                     </p>
                 </div>
             </div>

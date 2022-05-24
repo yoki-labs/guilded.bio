724a9e76a76c24e5cc699850744897a4a6f4d6c7
Nico <nico.03727+github@gmail.com>
2022-05-23 23:20:11 -0400

    test(API): take author as body prop

--- pages/api/users/[userId]/bios/index.tsx
+++ pages/api/users/[userId]/bios/index.tsx
@@ -7,12 +7,12 @@ const CreateDefaultBioRoute = async (req: NextApiRequest, res: NextApiResponse) 
         if (!content) return res.status(400).json({ error: { message: "You must provide content to set this bio to!" } });
 
         // "this is a placeholder for now until i get auth in place"
-        const nico = await prisma.user.findFirst({ where: { userId: "pmbOB8VA" } });
+        const author = await prisma.user.findFirst({ where: { id: req.body.author } });
         try {
             const bio = await prisma.bio.create({
                 data: {
                     content,
-                    authorId: nico!.id,
+                    authorId: author!.id,
                 },
             });
             await prisma.user.update({ where: { id: nico!.id }, data: { defaultBioId: bio.id } });

b06f23ac6c243a022e83461edde0d656c637dfa9
shay <43248357+shayypy@users.noreply.github.com>
2022-05-30 08:43:07 -0500

    refactor: `Number` instead of `parseInt`
    Co-authored-by: Dylan Warren <dylanwarren19@gmail.com>

--- pages/api/users/[userId]/bios/[bioId].tsx
+++ pages/api/users/[userId]/bios/[bioId].tsx
@@ -8,7 +8,7 @@ const BioRoute = async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession({ req });
     if (!session?.user) return Unauthenticated(res);
     
-    const bioId = parseInt(req.query.bioId as string);
+    const bioId = Number(req.query.bioId);
     if (Number.isNaN(bioId)) return BadRequest(res, "Invalid bio ID.");
 
     const bio = await prisma.bio.findFirst({ where: { id: bioId } }).catch(() => null);
